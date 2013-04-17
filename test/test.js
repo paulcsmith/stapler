67c005b5d7a66bd28263f996a5e70a4e9d32757c
Paul Smith <catalystmediastudios@gmail.com>
2013-04-17 16:58:38 -0600

    Cleans up the attached files after every test. Tests file copied correctly.

--- test/test.js
+++ test/test.js
@@ -1,25 +1,40 @@
 var should = require('should');
-var fs = require('fs');
+var fs = require('fs.extra');
 var collectionAttachment = require('../index.js');
-var path = './test/tmp/pic.jpg';
+var original_file_path = './test/tmp/pic.jpg';
 var name = 'pic.jpg.bkp';
+var config = {collection: 'products', id: '12345'};
+var path;
 var file;
 
 describe('Collection Attachment', function(){
   beforeEach(function copyTestFile () {
-    fs.createReadStream(path).pipe(fs.createWriteStream(path + '.bkp'));
-    path = path + '.bkp';
+    fs.createReadStream(original_file_path).pipe(fs.createWriteStream(original_file_path + '.bkp'));
+    path = original_file_path + '.bkp';
     file = {path: path, name: name};
   })
 
+  afterEach(function removeAttachedFile () {
+    fs.rmrfSync(process.cwd() + '/public');
+  })
+
   describe('attach(file, collection, id, callback)', function(){
-    it('should move original file to the correct default directory', function(done){
-      collectionAttachment.attach(file, {collection: 'products', id: '12345'}, function (err, file) {
+    it('should return the correct name and the new file path', function(done){
+      collectionAttachment.attach(file, config, function (err, new_file) {
         if (err) throw err;
-        file.path.should.equal('./public/uploads/products/12345/' + name);
-        file.name.should.equal(name);
+        new_file.path.should.equal('./public/uploads/products/12345/' + name);
+        new_file.name.should.equal(name);
+        done();
+      })
+    })
+
+    it('should save the file to the default directory', function (done) {
+      collectionAttachment.attach(file, config, function (err, new_file) {
+        if (err) throw err;
+        fs.existsSync(new_file.path).should.equal(true);
         done();
       })
     })
   })
+
 })
