34aa8e6be0849dc98e6469675f170656672edbc5
Paul Smith <catalystmediastudios@gmail.com>
2013-04-17 16:35:58 -0600

    Cleaned up the tests a bit

--- test/test.js
+++ test/test.js
@@ -1,15 +1,19 @@
 var should = require('should');
 var fs = require('fs');
 var collectionAttachment = require('../index.js');
+var path = './test/tmp/pic.jpg';
+var name = 'pic.jpg.bkp';
+var file;
 
 describe('Collection Attachment', function(){
+  beforeEach(function copyTestFile () {
+    fs.createReadStream(path).pipe(fs.createWriteStream(path + '.bkp'));
+    path = path + '.bkp';
+    file = {path: path, name: name};
+  })
+
   describe('attach(file, collection, id, callback)', function(){
     it('should move original file to the correct default directory', function(done){
-      var path = './test/tmp/pic.jpg';
-      fs.createReadStream(path).pipe(fs.createWriteStream(path + '.bkp'));
-      var name = 'pic.jpg.bkp';
-      path = path + '.bkp';
-      var file = {path: path, name: name};
       collectionAttachment.attach(file, {collection: 'products', id: '12345'}, function (err, file) {
         if (err) throw err;
         file.path.should.equal('./public/uploads/products/12345/' + name);
